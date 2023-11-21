8cf3762d62d0f1beb079daf62571d21c2f04dad6
agustinatahhan <agustahhan@gmail.com>
2023-11-21 18:30:39 -0300

    testing details

--- src/Views/Trials/Trials.jsx
+++ src/Views/Trials/Trials.jsx
@@ -18,23 +18,23 @@ const Trials = () => {
       <div className="container">
         <div className="row align-items-start justify-content-center">
           <div className="col-lg-6 text-center mb-4 mt-5" data-aos="fade-right">
-            <h2 className="display-4 fw-bold">{t("trials.title")}</h2>
-            <p className={`fs-5 fw-regular mb-4 ${style.descriptionText}`}>{t("trials.description")}</p>
+            <h2 className="display-4 fw-bold text-white">{t("trials.title")}</h2>
+            <p className={`fs-5 fw-regular my-4 text-white ${style.descriptionText}`}>{t("trials.description")}</p>
             <ul className="mt-4">
-              <li className="fs-5 fw-bold text-start mb-3">
+              <li className="fs-5 fw-bold text-start mb-3 text-white">
                 {" "}
                 {t("trials.yield")}
               </li>
-              <li className="fs-5 fw-bold text-start  mb-3">
+              <li className="fs-5 fw-bold text-start  mb-3 text-white">
                 {" "}
                 {t("trials.biomass")}
               </li>
-              <li className="fs-5 fw-bold text-start  mb-3">
+              <li className="fs-5 fw-bold text-start  mb-3 text-white">
                 {" "}
                 {t("trials.nodulation")}
               </li>
             </ul>
-            <p className={`fs-5 fst-italic mt-4`}>{t("trials.last")}</p>
+            <p className={`fs-5 fst-italic mt-4 text-white`}>{t("trials.last")}</p>
 
           </div>
           <div className={`col text-center mb-5 mx-2 `} data-aos="fade-left">
